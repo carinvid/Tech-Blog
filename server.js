1754dcf24263b600e30c7230e888e8998409e7c2
Carolina Reedy <carinvid@gmail.com>
2021-08-11 00:09:20 -0600

    handlebars

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const path = require("path");
 const routes = require("./controllers/");
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: "ricacookie",
-  cookie: {
-    // Session will expire in 15 minutes
-    expires: 15 * 60 * 1000,
-  },
+  cookie: {},
   resave: false,
-  rolling: true,
-  saveUninitialized: false,
+  saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
   }),
