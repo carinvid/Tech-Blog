9f5c3bcc440ec22bd364cfb4e1c1b7d2e34af1e9
Carolina Reedy <carinvid@gmail.com>
2021-08-10 23:51:40 -0600

    dep and apps

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const path = require("path");
 const routes = require("./controllers/");
-
+const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: "ricacookie",
+  cookie: {
+    // Session will expire in 15 minutes
+    expires: 15 * 60 * 1000,
+  },
+  resave: false,
+  rolling: true,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
