06d9c2b9aa34011b933cbbc2dd96cb2ef8ec705f
Carolina Reedy <carinvid@gmail.com>
2021-08-22 21:39:09 -0600

    fix typos

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 require("dotenv").config();
-
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW);
 // create connection to our db
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
   {
     host: "localhost",
     dialect: "mysql",
-    port: 3001,
+    port: 3306,
   }
 );
 
