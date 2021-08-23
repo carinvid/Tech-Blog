6fabb1d5e31e8e22e0478959a8445fd93cc3141d
Carolina Reedy <carinvid@gmail.com>
2021-08-22 21:45:46 -0600

    add files

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,25 +1,27 @@
-function signupFormHandler(event) {
+async function loginFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-  console.log(username, email, password);
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-  if (username && email && password) {
-    fetch("/api/users", {
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
       method: "post",
       body: JSON.stringify({
-        username,
         email,
         password,
       }),
       headers: { "Content-Type": "application/json" },
-    }).then((response) => {
-      console.log(response);
     });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
   }
 }
+
 document
-  .querySelector(".signup-form")
-  .addEventListener("click", signupFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
